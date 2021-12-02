8f973d3dd0ad231b025a23fa7f8917ef4118abe5
IamMasudMirzazada <masudjm@code.edu.az>
2021-12-02 12:42:52 -0800

    Task class

---
+++ homework-class/Class homework/Class homework/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_homework
+{
+    class Employee
+    {
+        public string Name;
+        public string Surname;
+        public string Position;
+        public int DailySalary;
+
+        public Employee(string name,string surname,string position,int dailysalary)
+        {
+            Name = name;
+            Surname = surname;
+            Position = position;
+            DailySalary = dailysalary;
+        }
+
+        public string Calc ()
+        {
+            
+            var result = $"{Name} {Surname} {Position} {DailySalary*30}" ;
+            return result;
+        }
+    }
+}
---
+++ homework-class/Class homework/Class homework/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Class_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person1 = new Employee("Kamran", "Jabiyev","Programmer",100);
+            Console.WriteLine(person1.Calc());
+        }
+    }
+}
